2ac8833c94bf90a87b02205b7f477bb7056675a0
cooper leibow <cleibow@tulane.edu>
2018-05-17 15:24:22 -0500

    added web api

---
+++ RealEstateCRM/RealEstateCRM.API/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using RealEstateCRM.DataAccessLayer;
+using RealEstateCRM.DataAccessLayer.Repositories;
+using RealEstateCRM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace RealEstateCRM.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        IRepository<User> crud;
+        IDbContext realEstateDb;
+
+        public UserController()
+        {
+            realEstateDb = new RealEstateCRMContext();
+            crud = new CRUD<User>(realEstateDb);
+
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            IEnumerable<User> allUsers = crud.Table;
+            return allUsers;
+        }
+
+        public IHttpActionResult GetUserById(int id)
+        {
+            var user = crud.GetByID(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
