948004d06c9dad38b6848e3db36747c09b078199
robertb100 <robert.borukhov@gmail.com>
2018-05-18 15:17:05 -0400

    Add unit test for Pipeline verification

---
+++ RealEstateCRMAPI/RealEstateCRM.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RealEstateCRM.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RealEstateCRM.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9700e0ca-d35a-40ef-99a1-69c8f01a3e16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RealEstateCRMAPI/RealEstateCRM.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RealEstateCRM.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int expected = 4;
+            int actual = 2 * 2;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
