5707a0524fcae5b879904794bb4bc49d2c06259e
louiswho <finalultimatenoob@gmail.com>
2018-05-28 20:13:50 -0400

    migrates changes to Test database

--- RealEstateCRMAPI/RealEstateCRM.DataAccessLayer/Migrations/Configuration.cs
+++ RealEstateCRMAPI/RealEstateCRM.DataAccessLayer/Migrations/Configuration.cs
@@ -5,15 +5,15 @@ namespace RealEstateCRM.DataAccessLayer.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<RealEstateCRM.DataAccessLayer.RealEstateCRMContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<RealEstateCRM.DataAccessLayer.TestCRMContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "RealEstateCRM.DataAccessLayer.RealEstateCRMContext";
+            ContextKey = "RealEstateCRM.DataAccessLayer.TestCRMContext";
         }
 
-        protected override void Seed(RealEstateCRM.DataAccessLayer.RealEstateCRMContext context)
+        protected override void Seed(RealEstateCRM.DataAccessLayer.TestCRMContext context)
         {
             //  This method will be called after migrating to the latest version.
 
